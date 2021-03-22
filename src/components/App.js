94af8b5ef297bfa41fe61fdf8f4aa269b38817ba
yannick.sprenger@gmail.com <yannick.sprenger@gmail.com>
2021-03-21 23:00:16 -0600

    redux functioning with an example screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Dashboard from './dashboard/Dashboard'
+import './App.css'
+
 
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
+    <Dashboard />
   )
 }
 
